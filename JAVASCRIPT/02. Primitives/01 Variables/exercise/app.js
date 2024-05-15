7718fb0df802ce3b151f7469a7d8aee22ddfd66d
Kirtigourab Gouda <98795248+kgourab73@users.noreply.github.com>
2024-05-15 21:52:30 +0530

    Add files via upload

---
+++ JAVASCRIPT/02. Primitives/01 Variables/exercise/app.js
@@ -0,0 +1,15 @@
+// 1. Create variable name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+
+let name = "Kirtigourab";
+let whatDoYouWannaBecomeInYourLife = "Web developer";
+let gender = "male";
+let Email = "kggouda732003@gmail.com";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(Email);
