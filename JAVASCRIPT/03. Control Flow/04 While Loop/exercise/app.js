8d7dfb2a8fa417e58556244a973168ee2cbca17d
Kirtigourab Gouda <98795248+kgourab73@users.noreply.github.com>
2024-05-16 23:24:34 +0530

    Add files via upload

---
+++ JAVASCRIPT/03. Control Flow/04 While Loop/exercise/app.js
@@ -0,0 +1,9 @@
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let i = 10;
+
+while (i <= 10) {
+  console.log("Kirtigourab Gouda");
+  i++;
+}
---
+++ JAVASCRIPT/05. Functions/02 Decleration & Expression/app.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function sayHello(username) {
+  console.log(`Hello ${username}`);
+}
+
+sayHello("Kirtigourab");
+
+// Function Expressions
+const greetings = function (user) {
+  console.log(`Hello ${user}`);
+};
+
+greetings("Sanu");
+
+// -------------------------
+console.log(x); // ERROR
+let x = 10;
