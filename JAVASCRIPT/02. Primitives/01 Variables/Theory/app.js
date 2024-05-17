85bbf83acaef8d17a0fbd71136c2b792ae7d230e
Kirtigourab Gouda <98795248+kgourab73@users.noreply.github.com>
2024-05-17 21:49:32 +0530

    Delete JAVASCRIPT/02. Primitives directory

--- JAVASCRIPT/02. Primitives/01 Variables/Theory/app.js
+++
@@ -1,7 +0,0 @@
-// (reserved keyword): (variable name): (type of value)
-
-let variable; // undefined
-let name = "Kirtigourab Gouda";
-console.log(name);
-const course = "Bechlore of Technology";
-console.log(course);
