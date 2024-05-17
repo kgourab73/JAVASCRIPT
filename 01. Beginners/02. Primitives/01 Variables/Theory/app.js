670c314c3dea27eca393b36f9cd89d8154cfb51a
Kirtigourab Gouda <98795248+kgourab73@users.noreply.github.com>
2024-05-17 22:00:16 +0530

    Add files via upload

---
+++ 01. Beginners/02. Primitives/01 Variables/Theory/app.js
@@ -0,0 +1,7 @@
+// (reserved keyword): (variable name): (type of value)
+
+let variable; // undefined
+let name = "Kirtigourab Gouda";
+console.log(name);
+const course = "Bechlore of Technology";
+console.log(course);
---
+++ 01. Beginners/02. Primitives/04 Comparison Operator/Theory/app.js
@@ -0,0 +1,23 @@
+// ***********************************
+// RELATIONAL OPERATORS
+// > Greater Then
+// < Less Then
+// >= Greater Then or equal to
+// <= Less Then or equal to
+
+console.log(8 > 7);
+console.log(8 < 7);
+console.log(8 >= 7);
+console.log(8 <= 7);
+
+// ***********************************
+// EQUALITY OPERATORS
+// === strict equality (Type + Value)
+// !== strict non-equality (Type + Value)
+// == Lose equality (values)
+// != Lose equality (values)
+
+console.log(8 === 7);
+console.log(8 !== 7);
+console.log(8 == 7);
+console.log(8 != 7);
